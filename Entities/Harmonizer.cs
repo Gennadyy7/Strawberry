6f8651863f85963806b2c324d65601891921eef4
Hvesko_Gennady_253502 <pprom5573@gmail.com>
2024-05-22 00:41:15 +0300

    add some harmonizer

--- Entities/Harmonizer.cs
+++ Entities/Harmonizer.cs
@@ -8,5 +8,61 @@ namespace Strawberry.Entities
 {
     public class Harmonizer
     {
+        public static Dictionary<Pitch, List<Note>> GenerateDiatonicTriads(Pitch tonic, HarmonyMode mode, int chordsPerBar, string instrument)
+        {
+            var diatonicTriads = new Dictionary<Pitch, List<Note>>();
+
+            int[] intervals;
+            switch (mode)
+            {
+                case HarmonyMode.Major:
+                    intervals = [0, 2, 4, 5, 7, 9, 11];
+                    break;
+                case HarmonyMode.Minor:
+                    intervals = [0, 2, 3, 5, 7, 8, 10];
+                    break;
+                case HarmonyMode.HarmonicMinor:
+                    intervals = [0, 2, 3, 5, 7, 8, 11];
+                    break;
+                default:
+                    throw new ArgumentException("Invalid harmony mode.");
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                var thirdPitch = tonic - intervals[i] - intervals[i + 2];
+                var fifthPitch = tonic - intervals[i] - intervals[i + 4];
+
+                if (i > 2)
+                {
+                    fifthPitch += 12;
+                    
+                    if (i > 4)
+                    {
+                        thirdPitch += 12;
+                    }
+                }
+
+                var triad = new List<Note>
+                {
+                    new Note(tonic - intervals[i] + 12, 16 / chordsPerBar, 0, 80, 0, instrument),
+                    new Note(tonic - intervals[i], 16 / chordsPerBar, 0, 80, 0, instrument),
+                    new Note(thirdPitch, 16 / chordsPerBar, 0, 80, 0, instrument),
+                    new Note(fifthPitch, 16 / chordsPerBar, 0, 80, 0, instrument)
+                };
+
+                diatonicTriads[tonic - intervals[i]] = triad;
+            }
+
+            return diatonicTriads;
+        }
+
+        public static void SetTriadPosition(List<Note> triad, int position)
+        {
+            foreach (var note in triad)
+            {
+                note.Position = position;
+            }
+        }
     }
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,31 +8,19 @@ namespace Strawberry
         public MainPage()
         {
             InitializeComponent();
-            Project = new Project("Test", 60);
+            Project = new Project("Test", 100);
             Track track = new Track("Simple Piano", "Piano");
-            track.AddNote(Pitch.E3, 1, 0, 100, 0);
-            track.AddNote(Pitch.E3, 1, 1, 100, 0);
-            track.AddNote(Pitch.E3, 1, 2, 100, 0);
-            track.AddNote(Pitch.E3, 1, 3, 100, 0);
-            track.AddNote(Pitch.E3, 1, 4, 100, 0);
-            track.AddNote(Pitch.E3, 1, 5, 100, 0);
-            track.AddNote(Pitch.E3, 1, 6, 100, 0);
-            track.AddNote(Pitch.E3, 1, 7, 100, 0);
-            track.AddNote(Pitch.E3, 1, 8, 100, 0);
-            track.AddNote(Pitch.E3, 1, 9, 100, 0);
-            track.AddNote(Pitch.E3, 1, 10, 100, 0);
-            track.AddNote(Pitch.E3, 1, 11, 100, 0);
-            track.AddNote(Pitch.E3, 1, 12, 100, 0);
-            track.AddNote(Pitch.E3, 1, 13, 100, 0);
-            track.AddNote(Pitch.E3, 1, 14, 100, 0);
-            track.AddNote(Pitch.E3, 1, 15, 100, 0);
-            Project.AddTrack(track);
-            Track track2 = new Track("Another Piano", "Piano");
-            track2.AddNote(Pitch.D3, 4, 0, 100, 0);
-            track2.AddNote(Pitch.D3, 4, 4, 100, 0);
-            track2.AddNote(Pitch.D3, 4, 8, 100, 0);
-            track2.AddNote(Pitch.D3, 4, 12, 100, 0);
-            Project.AddTrack(track2);
+            var chords = Harmonizer.GenerateDiatonicTriads(Pitch.C3, HarmonyMode.Major, 4, track.Instrument);
+            int i = 0;
+            foreach (var ch in chords)
+            {
+                Harmonizer.SetTriadPosition(ch.Value, i);
+                foreach (var n in ch.Value)
+                {
+                    track.AddNote(n);
+                }
+                i += 4;
+            }
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
